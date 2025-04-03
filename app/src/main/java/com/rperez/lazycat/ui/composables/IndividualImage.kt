0b266a8efa14a21604f97082d757f01edfc0860a
rick <richard19.perez77@yahoo.com>
2025-04-03 07:34:06 -0500

    use local image if saved

--- app/src/main/java/com/rperez/lazycat/ui/composables/IndividualImage.kt
+++ app/src/main/java/com/rperez/lazycat/ui/composables/IndividualImage.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -15,12 +14,10 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.rperez.lazycat.R
-import com.rperez.lazycat.utils.SaveImage
 
 @Composable
 fun IndividualImage(url: String) {
